b533b2185567e4be460d4d74a8091109888ba647
Neeraj Vashistha - <nv304@phobos.student.eecs.qmul.ac.uk>
2020-09-01 22:41:31 +0100

    python module setup script

---
+++ setup.py
@@ -0,0 +1,15 @@
+from inltk.inltk import setup
+import nltk
+import wget
+import zipfile
+
+setup('hi')
+nltk.download('punkt')
+nltk.download('averaged_perceptron_tagger')
+nltk.download('indian')
+nltk.download('stopwords')
+
+wget.download('https://ndownloader.figshare.com/files/24541397', out='model/model.zip')
+with zipfile.ZipFile('model/model.zip', 'r') as zip_ref:
+	zip_ref.extractall('model/')
+print('Uncompressing Completed!')
